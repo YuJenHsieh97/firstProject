96c22899853ce698ef1248c8f618dd21f17c104c
Yu Jen Hsieh <yhsieh@nyc.yearup.org>
2018-11-07 15:30:13 -0500

    First Project

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("Slides");
+  let i;
+  let slides = document.getElementsByClassName("Slides");
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); 
+    setTimeout(showSlides, 2000);
+}
+// console.log(window.scrollY);
+function enlarge(){
+
+  let plus = document.querySelector("#mybutton");
+  if (298 <= document.scrollY){
+    plus.className += " big";
+  }
 }
